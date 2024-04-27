ed27b05360a78afb9393d0a5e5877facc9e33d7b
danielferen52 <168289006+danielferen52@users.noreply.github.com>
2024-04-27 22:31:39 +0700

    Add files via upload

---
+++ src/config.js
@@ -0,0 +1,28 @@
+
+const mongoose = require('mongoose');
+const connect = mongoose.connect('mongodb+srv://nanangpemenang:adhetampan@cluster0.oq9hv2m.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0');
+
+// Check database connected or not
+connect.then(() => {
+    console.log("Database Connected Successfully");
+})
+.catch(() => {
+    console.log("Database cannot be Connected");
+})
+
+// Create Schema
+const Loginschema = new mongoose.Schema({
+    name: {
+        type:String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    }
+});
+
+// collection part
+const collection = new mongoose.model("users", Loginschema);
+
+module.exports = collection;
